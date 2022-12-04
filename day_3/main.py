6cf9ad4fcc69aebb5bd6f1b4fadfcf4af706e783
Vincent Dupont <vincent.dupont@trydatabook.com>
2022-12-04 12:04:25 +0100

    day 3 - part two

--- day_3/main.py
+++ day_3/main.py
@@ -1,6 +1,6 @@
 import string
 
-data = open("input.txt").readlines()
+data = [line.strip() for line in open("input.txt").readlines()]
 
 priorities_sum = 0
 
@@ -12,3 +12,17 @@ for line in data:
     priorities_sum += string.ascii_letters.index(list(common_items)[0]) + 1
 
 print(priorities_sum)
+assert priorities_sum == 7674
+
+GROUP_SIZE = 3
+
+priorities_sum = 0
+
+for offset in list(range(0, len(data), GROUP_SIZE)):
+    sack_a, sack_b, sack_c, *_ = data[offset:offset + GROUP_SIZE]
+    common_items = set(sack_a).intersection(set(sack_b)).intersection(set(sack_c))
+    assert len(common_items) == 1, f"should have only one item but got {common_items}"
+    priorities_sum += string.ascii_letters.index(list(common_items)[0]) + 1
+
+print(priorities_sum)
+assert priorities_sum == 2805
