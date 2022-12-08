66acb3ae38576107d278b2d8468e4be500a604d1
Vincent Dupont <samidarko@gmail.com>
2022-12-08 18:52:39 +0100

    day 8 - part two - draft

--- day_8/main.py
+++ day_8/main.py
@@ -11,15 +11,14 @@ def get_column(matrix, col_index):
     return [matrix[row_index][col_index] for row_index in range(len(matrix))]
 
 
-def main():
-    matrix = get_matrix()
+def part_one(matrix) -> int:
     total_visible = 0
     for row_index, row in enumerate(matrix):
         for col_index, tree_height in enumerate(row):
             if (
                     row_index == 0
-                    or row_index == len(matrix) - 1
                     or col_index == 0
+                    or row_index == len(matrix) - 1
                     or col_index == len(matrix) - 1
             ):
                 # all the trees around the edge of the grid are visible
@@ -42,7 +41,37 @@ def main():
             if all([item < tree_height for item in column[row_index + 1:]]):
                 total_visible += 1
                 continue
-    print(total_visible)
+    return total_visible
+
+
+def part_two(matrix) -> int:
+    max_scenic_score = 0
+    for row_index, row in enumerate(matrix):
+        for col_index, tree_height in enumerate(row):
+            if (
+                    row_index == 0
+                    or col_index == 0
+                    or row_index == len(matrix) - 1
+                    or col_index == len(matrix) - 1
+            ):
+                # all the trees around the edge of the grid are visible
+                continue
+
+            left_scene, right_scene = row[:col_index], row[col_index + 1:]
+
+            column = get_column(matrix, col_index)
+            down_scene, up_scene = column[:row_index], column[row_index + 1:]
+
+    return max_scenic_score
+
+
+def main():
+    matrix = get_matrix()
+    total_visible = part_one(matrix)
+    assert total_visible == 1796
+    scenic_score = part_two(matrix)
+    assert scenic_score == 0
+    print(scenic_score)
 
 
 if __name__ == "__main__":
