df2d968d3ebd24f3e0dd79e8f34623f00de56081
Vincent Dupont <vincent.dupont@trydatabook.com>
2022-12-08 22:36:26 +0100

    day 8 - part two

--- day_8/main.py
+++ day_8/main.py
@@ -44,6 +44,16 @@ def part_one(matrix) -> int:
     return total_visible
 
 
+def get_viewing_distance(trees, tree_height: int) -> int:
+    viewing_distance = 0
+    for height in trees:
+        viewing_distance += 1
+        if height >= tree_height:
+            break
+
+    return viewing_distance
+
+
 def part_two(matrix) -> int:
     max_scenic_score = 0
     for row_index, row in enumerate(matrix):
@@ -56,11 +66,29 @@ def part_two(matrix) -> int:
             ):
                 # all the trees around the edge of the grid are visible
                 continue
-
             left_scene, right_scene = row[:col_index], row[col_index + 1:]
+
+            right_viewing_distance = get_viewing_distance(right_scene, tree_height)
+
+            left_viewing_distance = get_viewing_distance(
+                reversed(left_scene), tree_height)
 
             column = get_column(matrix, col_index)
             down_scene, up_scene = column[:row_index], column[row_index + 1:]
+
+            down_viewing_distance = get_viewing_distance(
+                reversed(down_scene), tree_height)
+
+            up_viewing_distance = get_viewing_distance(up_scene, tree_height)
+
+            scenic_score = (
+                    left_viewing_distance
+                    * right_viewing_distance
+                    * down_viewing_distance
+                    * up_viewing_distance
+            )
+
+            max_scenic_score = max(max_scenic_score, scenic_score)
 
     return max_scenic_score
 
@@ -68,9 +96,10 @@ def part_two(matrix) -> int:
 def main():
     matrix = get_matrix()
     total_visible = part_one(matrix)
+    print(total_visible)
     assert total_visible == 1796
     scenic_score = part_two(matrix)
-    assert scenic_score == 0
+    assert scenic_score == 288120
     print(scenic_score)
 
 
